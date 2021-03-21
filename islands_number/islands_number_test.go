1fc05a68a9b0092ae3d9d8518dc5160a736ae165
Daniele Stefano Ferru <ferruvich@gmail.com>
2021-03-21 10:50:57 +0000

    adding tests to islands_number package

--- islands_number/islands_number_test.go
+++ islands_number/islands_number_test.go
@@ -1,1 +1,43 @@
-package islands_number
+package islands_number_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/ferruvich/training-leetcode/islands_number"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNumIslands(t *testing.T) {
+	for _, tt := range []struct {
+		grid          [][]byte
+		islandsNumber int
+	}{
+		{
+			grid: [][]byte{
+				{'1', '1', '1', '1', '0'},
+				{'1', '1', '0', '1', '0'},
+				{'1', '1', '0', '0', '0'},
+				{'0', '0', '0', '0', '0'},
+			},
+			islandsNumber: 1,
+		},
+		{
+			grid: [][]byte{
+				{'1', '1', '0', '0', '0'},
+				{'1', '1', '0', '0', '0'},
+				{'0', '0', '1', '0', '0'},
+				{'0', '0', '0', '1', '1'},
+			},
+			islandsNumber: 3,
+		},
+	} {
+		t.Run(
+			fmt.Sprintf("should return correctly spot %d islands with %v grid", tt.islandsNumber, tt.grid),
+			func(t *testing.T) {
+				n := islands_number.NumIslands(tt.grid)
+				assert.Equal(t, tt.islandsNumber, n)
+			},
+		)
+	}
+}
